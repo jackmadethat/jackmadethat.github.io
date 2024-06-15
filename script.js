b2c6d65139a68b8dde32f1d35428d75f0d6a634e
jackmadethat <underscore@gmx.com>
2024-06-16 08:47:26 +0930

    updates

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-window.onload = function() {
-    document.getElementById("expand").checked = false;
- }
+const dice = [
+    "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_1.png", 
+    "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_2.png", 
+    "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_3.png",
+    "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_4.png", 
+    "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_5.png", 
+    "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_6.png",
+];
 
+intervalId = setInterval(() => {
+    currentIndex = Math.floor(Math.random() * dice.length);
+    document.getElementById("diceimage").src = dice[currentIndex];
+  }, 100);
  
