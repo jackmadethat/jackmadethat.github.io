b42e8fe8f2f960aee6cc173f83ba4a916c70671f
jackmadethat <underscore@gmx.com>
2024-06-16 09:11:22 +0930

    update diceroller

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const rollBtn = document.getElementById("rollButton");
+
 const dice = [
     "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_1.png", 
     "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_2.png", 
@@ -7,8 +9,33 @@ const dice = [
     "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/dice/Dice_6.png",
 ];
 
-intervalId = setInterval(() => {
-    currentIndex = Math.floor(Math.random() * dice.length);
-    document.getElementById("diceimage").src = dice[currentIndex];
-  }, 60);
- 
+let currentIndex = 0;
+let intervalId = null;
+const startTime = new Date();
+
+function rapidCycle() {
+    intervalId = setInterval(() => {
+      currentIndex = Math.floor(Math.random() * dice.length);
+      document.getElementById("diceimage").src = dice[currentIndex];
+    }, 100);
+  }
+
+function slowDown() {
+    clearInterval(intervalId);
+    let slowIntervalId = setInterval(() => {
+      const remainingTime = 5000 - (new Date() - startTime); // Calculate remaining time
+      const delay = 100 + (remainingTime / 10); // Increase delay for each remaining millisecond
+      currentIndex = Math.floor(Math.random() * dice.length);
+      document.getElementById("diceimage").src = dice[currentIndex];
+      if (remainingTime <= 0) {
+        clearInterval(slowIntervalId);
+      }
+    }, 25);
+  }
+  
+function roll() {
+  rapidCycle();
+  setTimeout(slowDown, 5000);
+}
+  
+rollBtn.addEventListener("click", roll);
