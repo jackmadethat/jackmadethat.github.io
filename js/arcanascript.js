f5cf6fd579ea559f0634846c9c8f14b1f40219ea
jackmadethat <underscore@gmx.com>
2024-06-18 07:30:59 +0930

    initial test

--- js/arcanascript.js
+++ js/arcanascript.js
@@ -3,3 +3,32 @@ JackMadeThat Tarot Deck
 Handle a digital arcana
 */
 
+const image = document.getElementById("card");
+
+image.addEventListener("mousedown", startDrag);
+image.addEventListener("mouseup", stopDrag);
+
+let mouseX, mouseY, imageX, imageY, isDragging = false;
+
+function startDrag(event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+  imageX = image.offsetLeft;
+  imageY = image.offsetTop;
+  isDragging = true;
+  console.log("test");
+}
+
+function stopDrag() {
+  isDragging = false;
+  console.log("test");
+}
+
+document.addEventListener("mousemove", (event) => {
+  if (isDragging) {
+    const newX = imageX + (event.clientX - mouseX);
+    const newY = imageY + (event.clientY - mouseY);
+    image.style.top = `${newY}px`;
+    image.style.left = `${newX}px`;
+  }
+});
