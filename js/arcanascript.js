4371da5b557c1ad3f1c76010e78dbc18bb1a9d62
jackmadethat <underscore@gmx.com>
2024-06-18 07:35:40 +0930

    testing mobile

--- js/arcanascript.js
+++ js/arcanascript.js
@@ -7,6 +7,14 @@ const image = document.getElementById("card");
 
 image.addEventListener("mousedown", startDrag);
 image.addEventListener("mouseup", stopDrag);
+image.addEventListener("touchstart", startDrag);
+image.addEventListener("touchend", stopDrag);
+document.addEventListener("touchmove", (event) => {
+  if (isDragging) {
+    event.preventDefault();
+    moveImage(event.touches[0].clientX, event.touches[0].clientY);
+  }
+});
 
 let mouseX, mouseY, imageX, imageY, isDragging = false;
 
