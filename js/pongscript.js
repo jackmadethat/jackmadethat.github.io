de893daf24733a3ddad21da08a1206a24b6af01e
Jack Beven <underscore@gmx.com>
2024-06-28 09:12:26 +0930

    Attempt to cleanup code so far

--- js/pongscript.js
+++ js/pongscript.js
@@ -50,23 +50,19 @@ court.addEventListener('mousemove', (event) => {
 // -----
 
 const animate = () => {
-  const initialMarginLeft = 0;
-  const initialMarginTop = 0;
-  const initialWidth = 800;
-  const initialHeight = 600;
-  const middleMarginLeft = 266;
-  const middleMarginTop = 200;
-  const middleWidth = 266;
-  const middleHeight = 200;
-  const finalMarginLeft = 0;
-  const finalMarginTop = 0;
-  const finalWidth = 800;
-  const finalHeight = 600;
-  const ballInitialWidth = 60;
-  const ballInitialHeight = 60;
-  const ballMiddleWidth = 15;
-  const ballMiddleHeight = 15;
-  const duration = 4000; // 2 seconds
+  const topMarginLeft = 0;
+  const topMarginTop = 0;
+  const topWidth = 800;
+  const topHeight = 600;
+  const bottomMarginLeft = 266;
+  const bottomMarginTop = 200;
+  const bottomWidth = 266;
+  const bottomHeight = 200;
+  const ballTopWidth = 60;
+  const ballTopHeight = 60;
+  const ballBottomWidth = 15;
+  const ballBottomHeight = 15;
+  const duration = 4000;
 
   let currentTime = 0;
 
@@ -74,33 +70,25 @@ const animate = () => {
     const progress = currentTime / duration;
     let marginLeft, marginTop, width, height, ballWidth, ballHeight;
 
-
     if (progress < 0.5) {
-      // First half of the animation
-      marginLeft = initialMarginLeft + (middleMarginLeft - initialMarginLeft) * progress * 2;
-      marginTop = initialMarginTop + (middleMarginTop - initialMarginTop) * progress * 2;
-      width = initialWidth + (middleWidth - initialWidth) * progress * 2;
-      height = initialHeight + (middleHeight - initialHeight) * progress * 2;
-      ballWidth = ballInitialWidth + (ballMiddleWidth - ballInitialWidth) * progress * 2;
-      ballHeight = ballInitialHeight + (ballMiddleHeight - ballInitialHeight) * progress * 2;
+		// First half of the animation
+		depthMarker.style.marginLeft = `${topMarginLeft + (bottomMarginLeft - topMarginLeft) * progress * 2}px`;
+		depthMarker.style.marginTop = `${topMarginTop + (bottomMarginTop - topMarginTop) * progress * 2}px`;
+		depthMarker.style.width = `${topWidth + (bottomWidth - topWidth) * progress * 2}px`;
+		depthMarker.style.height = `${topHeight + (bottomHeight - topHeight) * progress * 2}px`;
+		ball.style.width = `${ballTopWidth + (ballBottomWidth - ballTopWidth) * progress * 2}px`;
+		ball.style.height = `${ballTopHeight + (ballBottomHeight - ballTopHeight) * progress * 2}px`;
     } else {
-      // Second half of the animation
-      marginLeft = middleMarginLeft + (finalMarginLeft - middleMarginLeft) * (progress - 0.5) * 2;
-      marginTop = middleMarginTop + (finalMarginTop - middleMarginTop) * (progress - 0.5) * 2;
-      width = middleWidth + (finalWidth - middleWidth) * (progress - 0.5) * 2;
-      height = middleHeight + (finalHeight - middleHeight) * (progress - 0.5) * 2;
-      ballWidth = ballMiddleWidth + (ballInitialWidth - ballMiddleWidth) * (progress - 0.5) * 2;
-      ballHeight = ballMiddleHeight + (ballInitialHeight - ballMiddleHeight) * (progress - 0.5) * 2;
+		// Second half of the animation
+		depthMarker.style.marginLeft = `${bottomMarginLeft + (topMarginLeft - bottomMarginLeft) * (progress - 0.5) * 2}px`;
+		depthMarker.style.marginTop = `${bottomMarginTop + (topMarginTop - bottomMarginTop) * (progress - 0.5) * 2}px`;
+		depthMarker.style.width = `${bottomWidth + (topWidth - bottomWidth) * (progress - 0.5) * 2}px`;
+		depthMarker.style.height = `${topHeight + (bottomHeight + (topHeight - bottomHeight) * (progress - 0.5) * 2}px`;
+		ball.style.width = `${ballBottomWidth + (ballTopWidth - ballBottomWidth) * (progress - 0.5) * 2}px`;
+		ball.style.height = `${ballBottomHeight + (ballTopHeight - ballBottomHeight) * (progress - 0.5) * 2}px`;
     }
-
-    depthMarker.style.marginLeft = `${marginLeft}px`;
-    depthMarker.style.marginTop = `${marginTop}px`;
-    depthMarker.style.width = `${width}px`;
-    depthMarker.style.height = `${height}px`;
-    ball.style.width = `${ballWidth}px`;
-    ball.style.height = `${ballHeight}px`;
-    const ballOffsetX = (ballInitialWidth - ballWidth) / 2;
-    const ballOffsetY = (ballInitialHeight - ballHeight) / 2;
+    const ballOffsetX = (ballTopWidth - ballWidth) / 2;
+    const ballOffsetY = (ballTopHeight - ballHeight) / 2;
     ball.style.transform = `translate(${ballOffsetX + 370}px, ${ballOffsetY + 270}px)`;
 
     currentTime += 20; // increment time by 20ms
