16b0d09bda070bfc95a19194517b992eca57efef
jackmadethat <underscore@gmx.com>
2024-06-28 06:41:24 +0930

    various minor adds and fixes

--- js/pongscript.js
+++ js/pongscript.js
@@ -9,12 +9,16 @@ Play a game of 3D Pong!
 
 const court = document.getElementById('court');
 const ball = document.getElementById('ball');
+const paddle = document.getElementById('paddle');
+const scoreBox = document.getElementById('score');
+const highScoreBox = document.getElementById('highest-score');
 
 // -----
 // Variables
 // -----
 
-
+let score = 0;
+let highestScore = 0;
 
 // -----
 // Animate the ball
