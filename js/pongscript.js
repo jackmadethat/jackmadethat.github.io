c4fc5dec10788c8d050997b2a7f5f2d4170ce215
jackmadethat <underscore@gmx.com>
2024-06-28 06:15:37 +0930

    Setup 3D Pong files

---
+++ js/pongscript.js
@@ -0,0 +1,22 @@
+/*
+JackMadeThat 3D Pong Script
+Play a game of 3D Pong!
+*/
+
+// -----
+// Components
+// -----
+
+const court = document.getElementById('court');
+const ball = document.getElementById('ball');
+
+// -----
+// Variables
+// -----
+
+
+
+// -----
+// Animate the ball
+// -----
+
