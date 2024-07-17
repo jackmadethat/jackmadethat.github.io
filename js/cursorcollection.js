d53c126ff546378f70304a8592411266fcb648e8
jackmadethat <underscore@gmx.com>
2024-07-18 07:26:49 +0930

    Basic fun built

---
+++ js/cursorcollection.js
@@ -0,0 +1,22 @@
+/*
+JackMadeThat Custom Cursor Arrays
+*/
+
+const AirStrike = [
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_01.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_02.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_03.png",
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_04.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_05.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_06.png",
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_07.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_08.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_09.png",
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_10.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_11.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_12.png",
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_13.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_14.png", 
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_15.png",
+  "https://raw.githubusercontent.com/jackmadethat/jackmadethat.github.io/main/img/cursorset/Cursor_Airstrike_16.png"
+];
--- js/cursorscript.js
+++ js/cursorscript.js
@@ -1,22 +1,58 @@
 /*
-JackMadeThat Home Page Javascript
+JackMadeThat Custom Cursor Set
 */
 
 // -----
 // Components
 // -----
 
+const cursorDiv = document.getElementById('cursorImg');
 const cursorImg = document.getElementById('cursorImg');
 
 // -----
-// Hover events
+// Variables
+// -----
+
+let index = 0;
+let interval = 100;
+let lastTime = 0;
+let imgArray = [];
+
+// -----
+// Track Cursor
 // -----
 
 document.addEventListener('mousemove', (e) => {
   e.preventDefault();
   const x = e.clientX;
   const y = e.clientY;
-  cursorImg.style.left = `${x - 16}px`;
-  cursorImg.style.top = `${y - 16}px`;
+  cursorDiv.style.left = `${x - 16}px`;
+  cursorDiv.style.top = `${y - 16}px`;
   document.body.style.cursor = 'none';
 });
+
+// -----
+// Hover Events
+// -----
+
+
+
+// -----
+// Animate Cursor
+// -----
+
+imgArray = AirStrike;
+
+const animate = (timestamp) => {
+  if (timestamp - lastTime >= interval) {
+    cursorImg.src = imgArray[index];
+    index = (index + 1) % imgArray.length;
+    if (index === 0) { // reset index when reaching the end
+      index = 0;
+    }
+    lastTime = timestamp;
+  }
+  requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
