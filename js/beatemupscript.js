94262bfd2d6bd04ca9b1fb4c86f06abb2a1b7a64
jackmadethat <underscore@gmx.com>
2024-08-01 07:18:06 +0930

    Update beatemupscript.js

--- js/beatemupscript.js
+++ js/beatemupscript.js
@@ -14,46 +14,32 @@ const fighter = document.getElementById('fighterDiv');
 
 let playerX = 0;
 let playerY = 0;
-let velocityX = 0;
-let velocityY = 0;
 const speed = 2;
+const keys = {
+	ArrowUp: false,
+	ArrowDown: false,
+	ArrowLeft: false,
+	ArrowRight: false
+};
 
 function update() {
-  playerX += velocityX;
-  playerY += velocityY;
-  fighter.style.top = `${playerY}px`;
-  fighter.style.left = `${playerX}px`;
-  requestAnimationFrame(update);
+	if (keys.ArrowUp) playerY -= speed;
+	if (keys.ArrowDown) playerY += speed;
+	if (keys.ArrowLeft) playerX -= speed;
+	if (keys.ArrowRight) playerX += speed;
+
+	fighter.style.top = `${playerY}px`;
+	fighter.style.left = `${playerX}px`;
+	
+	requestAnimationFrame(update);
 }
 
 document.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'ArrowUp':
-      velocityY = -speed;
-      break;
-    case 'ArrowDown':
-      velocityY = speed;
-      break;
-    case 'ArrowLeft':
-      velocityX = -speed;
-      break;
-    case 'ArrowRight':
-      velocityX = speed;
-      break;
-  }
+	keys[event.key] = true;
 });
 
 document.addEventListener('keyup', (event) => {
-  switch (event.key) {
-    case 'ArrowUp':
-    case 'ArrowDown':
-      velocityY = 0;
-      break;
-    case 'ArrowLeft':
-    case 'ArrowRight':
-      velocityX = 0;
-      break;
-  }
+	keys[event.key] = false;
 });
 
 update();
