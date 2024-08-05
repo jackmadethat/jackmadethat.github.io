bf32b0669d13cc6bab7f86be8115b9a5bb76c2fd
jackmadethat <underscore@gmx.com>
2024-08-05 11:43:54 +0930

    beatemup updates
    finally got the modifier key working as intended!

--- js/beatemupscript.js
+++ js/beatemupscript.js
@@ -9,37 +9,73 @@ JackMadeThat Beat 'em Up Script
 const fighter = document.getElementById('fighterDiv');
 
 // -----
-// Move Character
+// Variables
 // -----
 
 let playerX = 0;
 let playerY = 0;
 const speed = 2;
-const keys = {
-	ArrowUp: false,
-	ArrowDown: false,
-	ArrowLeft: false,
-	ArrowRight: false
-};
+const sprintSpeed = 5;
+const keys = {};
+let currentSpeed = speed;
+let isShiftDown = false
 
-function update() {
-	if (keys.ArrowUp) playerY -= speed;
-	if (keys.ArrowDown) playerY += speed;
-	if (keys.ArrowLeft) playerX -= speed;
-	if (keys.ArrowRight) playerX += speed;
+// -----
+// Move Character
+// -----
 
+const update = () => {
+
+	// Keypress functionality
+	if (isShiftDown) {
+		currentSpeed = sprintSpeed;
+	} else {
+		currentSpeed = speed;
+	}
+
+	if (keys.ArrowUp || keys.w || keys.W) playerY -= currentSpeed;
+
+	if (keys.ArrowDown || keys.s || keys.S) playerY += currentSpeed;
+
+	if (keys.ArrowLeft || keys.a || keys.A) {
+		playerX -= currentSpeed;
+		fighter.style.transform = 'scaleX(-1)';
+	}
+
+	if (keys.ArrowRight || keys.d || keys.D) {
+		playerX += currentSpeed;
+		fighter.style.transform = 'scaleX(1)';
+	}
+  
+	// Set the player position
 	fighter.style.top = `${playerY}px`;
 	fighter.style.left = `${playerX}px`;
-	
+  
+	// Update frame
 	requestAnimationFrame(update);
+	console.log(keys);
 }
 
-document.addEventListener('keydown', (event) => {
-	keys[event.key] = true;
-});
+// -----
+// Keyboard Events
+// -----
 
-document.addEventListener('keyup', (event) => {
-	keys[event.key] = false;
+document.addEventListener('keydown', (event) => {
+	keys[event.key.toLowerCase()] = true;
+
+	isShiftDown = event.getModifierState('Shift');
 });
+  
+document.addEventListener('keyup', (event) => {
+	keys[event.key.toLowerCase()] = false;
+
+	if (event.key === 'Shift') {
+		isShiftDown = false;
+	}
+});
+  
+// -----
+// Start Game Loop
+// -----
 
 update();
