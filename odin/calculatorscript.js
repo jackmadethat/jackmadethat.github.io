0833384dfb14f60fc187d1d4da1dd1982bfd0ea3
jackmadethat <jbeven@ekium.com.au>
2025-07-07 12:49:21 +0930

    finished calculator, I think

--- odin/calculatorscript.js
+++ odin/calculatorscript.js
@@ -2,56 +2,114 @@ let textDisplay = document.getElementById("textDisplay");
 let firstNumber = "";
 let secondNumber = "";
 let operated = false;
+let operator = "";
+let completed = false;
 
 const enterDigit = (int) => {
+    if (completed) {
+        clear();
+    }
     if (!operated) {
         textDisplay.insertAdjacentHTML('beforeend', int);
         firstNumber = firstNumber.concat(int);
-        console.log(firstNumber);
     } else {
         textDisplay.insertAdjacentHTML('beforeend', int);
         secondNumber = secondNumber.concat(int);
-        console.log(secondNumber);
     }
+}
+
+const produceAnswer = (x, operation, y) => {
+    let result = 0;
+    if (operated) {
+
+        finalX = parseFloat(x);
+        finalY = parseFloat(y);
+
+        switch (operation) {
+            case "+":
+                result = finalX + finalY;
+                break;
+            case "-":
+                result = finalX - finalY;
+                break;
+            case "*":
+                result = finalX * finalY;
+                break;
+            case "/":
+                if (finalX == 0 || finalY == 0) {
+                    textDisplay.innerText = "Don't do that!";
+                    return;
+                } else {
+                    result = finalX / finalY;
+                }
+                break;
+        }
+
+        completed = true;
+        console.log(result);
+        textDisplay.insertAdjacentHTML('beforeend', Number(result.toFixed(2)));
+    }
+}
+
+const clear = () => {
+    operator = "";
+    firstNumber = "";
+    secondNumber = "";
+    operated = false;
+    textDisplay.innerHTML = "";
+    completed = false;
 }
 
 const func = (command) => {
     switch (command) {
         case "add":
-            if (!operated) {
+            if (completed) {
+                clear();
+            }
+            if (!operated || firstNumber != "") {
                 textDisplay.insertAdjacentHTML('beforeend', " + ");
-                console.log("+");
+                operator = "+";
                 operated = true;
             }
             break;
         case "subtract":
-            if (!operated) {
+            if (completed) {
+                clear();
+            }
+            if (!operated || firstNumber != "") {
                 textDisplay.insertAdjacentHTML('beforeend', " - ");
-                console.log("-");
+                operator = "-";
                 operated = true;
             }
             break;
         case "multiply":
-            if (!operated) {
+            if (completed) {
+                clear();
+            }
+            if (!operated || firstNumber != "") {
                 textDisplay.insertAdjacentHTML('beforeend', " x ");
-                console.log("*");
+                operator = "*";
                 operated = true;
             }
             break;
         case "divide":
-            if (!operated) {
+            if (completed || firstNumber != "") {
+                clear();
+            }
+            if (!operated || firstNumber != "") {
                 textDisplay.insertAdjacentHTML('beforeend', " &#247; ");
-                console.log("/");
+                operator = "/";
                 operated = true;
             }
             break;
         case "clear":
-            console.log("C");
-            textDisplay.innerHTML = "";
+            clear();
             break;
         case "equals":
-            textDisplay.insertAdjacentHTML('beforeend', " = ");
-            console.log("=");
+            if (firstNumber != "" && secondNumber != "") {
+                textDisplay.insertAdjacentHTML('beforeend', " = ");
+                produceAnswer(firstNumber, operator, secondNumber);
+            }
             break;
     }
 }
