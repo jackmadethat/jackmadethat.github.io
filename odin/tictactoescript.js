46926d328fd0dffef3c8fe59fb0a24e24ae762b4
jackmadethat <jbeven@ekium.com.au>
2025-07-11 14:32:02 +0930

    started tictactoe project

---
+++ odin/tictactoescript.js
@@ -0,0 +1,21 @@
+let gameBoard = {
+  top: ["", "", ""],
+  middle: ["", "", ""],
+  bottom: ["", "", ""]
+}
+
+let game = true; // true if game is active
+
+let turn = true; // X is true, O is false
+
+const selectSquare = (r, n) => {
+ console.log(r, n);
+}
+
+const renderBoard = () => {
+
+}
+
+const checkForWin = () => {
+
+}
