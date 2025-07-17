c42fc7eafb54c01063656211378bbc41f437faa6
jackmadethat <jbeven@ekium.com.au>
2025-07-17 11:20:19 +0930

    Finished weather app

--- odin/weatherscript.js
+++ odin/weatherscript.js
@@ -1,9 +1,41 @@
-const img = document.getElementById("container");
+const container = document.getElementById("container");
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=QCZJkcdvqouXQtRfuYWQKN9msixZoHxV&s=fail', {mode: 'cors'})
-.then(function(response) {
-  return response.json();
-})
-.then(function(response) {
-  img.src = response.data.images.original.url;
-});
+const getWeather = () => {
+  let location = document.getElementById("enter_location").value;
+  container.innerHTML = "<h2>Loading...</h2>";
+  fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=DD7JTQ8XYFT57SH333Q6H4AM6&contentType=json`, {mode: 'cors'})
+  .then(function(response) {
+    if (response.status == 400) {
+      container.innerHTML = "<h2>Invalid City!</h2>";
+    }
+    return response.json();
+  })
+  .then(function(response) {
+    displayWeather(response);
+  })
+}
+
+const displayWeather = (response) => {
+  container.innerHTML = `
+    <h2>Weather in ${response.address}</h2>
+    <h3>Overview</h3>
+    <ul>
+      <li>Time: ${response.currentConditions.datetime}</li>
+      <li>Today's Date: ${new Date().toDateString()}</li>
+      <li>Temperature: ${response.currentConditions.temp}F</li>
+      <li>Humidity: ${response.currentConditions.humidity}F</li>
+      <li>Current Conditions: ${response.currentConditions.conditions}</li>
+    </ul>
+    <p>${response.description}</p>
+    <h3>Astronomy</h3>
+    <ul>
+      <li>Sunrise: ${response.currentConditions.sunrise}</li>
+      <li>Sunset: ${response.currentConditions.sunset}</li>
+      <li>Moonphase: ${response.currentConditions.moonphase}</li>
+    </ul>
+    <ul>
+      <li>Latitude: ${response.latitude}</li>
+      <li>Longitude: ${response.longitude}</li>
+    </ul>
+  `;
+}
