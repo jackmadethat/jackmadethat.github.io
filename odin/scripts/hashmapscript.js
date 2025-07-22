dc5168c2dfb634a86257c12db3db5c721d419c15
jackmadethat <jbeven@ekium.com.au>
2025-07-22 12:14:45 +0930

    finished hash map script

--- odin/scripts/hashmapscript.js
+++ odin/scripts/hashmapscript.js
@@ -5,17 +5,17 @@ function HashMap() {
 
   this.hash = function hash(key) {
     let hash = 0;
+    let hashLength = 4; // adjust to change character length of hash
     const primeNumber = 31;
 
-    for (let i = 0; i < key.length; i++) {
+    for (let i = 0; i < Math.min(key.length, hashLength); i++) {
       hash = primeNumber * hash + key.charCodeAt(i) % this.size;
     };
-
-    //console.log("Hash: " + hash);
+    // console.log("Hash: " + hash);
     return hash;
   }
 
-  this.set = function put(key, value) {
+  this.set = function set(key, value) {
     const index = this.hash(key);
     if (!this.buckets[index]) {
       this.buckets[index] = [];
@@ -70,21 +70,71 @@ function HashMap() {
   this.numkeys = function numkeys() {
     return ("There are " +  this.keys + " keys and the size is " + this.size);
   }
+
+  this.keylist = function keylist() {
+    let keyArray = [];
+    for (let bucket of this.buckets) {
+      if (bucket) {
+        for (let pair of bucket) {
+          keyArray.push(pair[0]);
+        }
+      }
+    }
+    return ("Here's an array of the keys: " + keyArray);
+  }
+
+  this.valuelist = function valuelist() {
+    let valueArray = [];
+    for (let bucket of this.buckets) {
+      if (bucket) {
+        for (let pair of bucket) {
+          valueArray.push(pair[1]);
+        }
+      }
+    }
+    return ("Here's an array of the values: " + valueArray);
+  }
+
+  this.entries = function entries() {
+    let entryArray = [];
+    for (let bucket of this.buckets) {
+      if (bucket) {
+        for (let pair of bucket) {
+          entryArray.push(pair[0], pair[1]);
+        }
+      }
+    }
+    return ("Here's an array of the entries: " + entryArray);
+  }
+
+  this.clear = function clear() {
+    this.buckets = new Array(this.size).fill(undefined);
+    this.keys = 0;
+    return "Hashmap has been emptied. Keys: " + this.keys;
+  }
 }
 
 let map = new HashMap();
 
-map.set("apple", "an apple is not a berry");
-map.set("banana", "bananas are a berry");
-map.set("peach", "peach is a stone fruit");
-map.set("space cakes", "space cakes probably don't exist");
-console.log(map.get("apple"));
-map.remove("apple");
-console.log(map.get("apple"));
-console.log(map.get("banana"));
+map.set("sonic", "a well-known video game hedgehog");
+map.set("mario", "ostensibly a plumber");
+map.set("peach", "mario's main squeeze");
+map.set("master chief", "also known as 'John'");
+map.set("solar jetman", "probably the best NES game ever made");
+map.set("yoshi", "mario's best friend or sacrificial lamb");
+map.set("dr gordon freeman", "the mute hero of the resonance cascade tragedy");
+map.set("doomguy", "a man literally too angry to die");
+
+console.log(map.get("sonic"));
+map.remove("sonic");
+console.log(map.get("sonic"));
 console.log(map.get("peach"));
+console.log(map.get("master chief"));
 console.log(map.get("orange"));
-console.log(map.get("space cakes"));
 console.log(map.numkeys());
-console.log(map.has("apple"));
-console.log(map.has("banana"));
+console.log(map.has("sonic"));
+console.log(map.has("mario"));
+console.log(map.keylist());
+console.log(map.valuelist());
+console.log(map.entries());
+console.log(map.clear());
